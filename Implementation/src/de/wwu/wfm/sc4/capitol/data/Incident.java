f873e7dade7f2464c96dcb3e4bfb8152503d786b
the-Other-One <jan.dagefoerde@googlemail.com>
2014-01-07 12:54:49 +0100

    enhance Incident data class
    ... under the assumption that we are NOT using hibernate. If we are, all
    the ..ID fields can be left out (which is why I will stop doing this
    now, as most of this might be deleted soon ;) )

--- Implementation/src/de/wwu/wfm/sc4/capitol/data/Incident.java
+++ Implementation/src/de/wwu/wfm/sc4/capitol/data/Incident.java
@@ -1,18 +1,51 @@
 package de.wwu.wfm.sc4.capitol.data;
 
 import java.io.Serializable;
-import java.util.Collection;
+import java.util.List;
 
 public class Incident implements Serializable{
+	
+	private Integer incidentID;
 
-	private Collection<Contract> contract;
+	private Contract contract;
+	private Integer contractID;
 
-	private Collection<Car> car;
+	private Car car;
+	private Integer carID;
 
 	private AccidentReport accidentReport;
+	private Integer accidentReportID; 
 
 	private DamageReport damageReport;
+	private Integer damageReportID;
 
-	private Invoice invoice;
+	private List<Invoice> invoices;
 
+	public Contract getContract() {
+		if (contract != null) 
+			return contract;
+		
+		if (contractID != null) {
+			// TODO
+			// contract = ContractService.getSingleton().getById(contractID);
+			// return contract;
+		}
+		
+		return null;
+	}
+	
+	public List<Invoice> getInvoices() {
+		if (invoices != null)
+			return invoices;
+		
+		if (incidentID == null) {
+			// incident is not saved yet, therefore there are no related invoices!
+			return null;	
+		}
+		
+		// TODO
+		//invoices = InvoiceService.getSingleton().getAllByIncidentId(incidentID);
+		return invoices;
+	}
+	
 }
