b6128facdca3b8733a346ae85c7749ac6449d612
Dexter-03 <tobias.reischmann@googlemail.com>
2014-01-08 22:33:37 +0100

    Working state of the BuildInsuranceContract

--- Implementation/src/de/wwu/wfm/sc4/capitol/contractnegotiation/apps/BuildInsuranceContract.java
+++ Implementation/src/de/wwu/wfm/sc4/capitol/contractnegotiation/apps/BuildInsuranceContract.java
@@ -5,17 +5,26 @@ import de.wwu.wfm.sc4.capitol.data.Contract;
 import de.wwu.wfm.sc4.capitol.data.Requirements;
 
 public class BuildInsuranceContract {
+	
+	private Case contractingCase;
+	
+	private Contract contract;
+	
+	private Requirements customerRequirements;
+	
 	public BuildInsuranceContract() {
 
 	}
 	public void setCustomerRequirements(Requirements customerRequirements){
-		
+		this.customerRequirements=customerRequirements;
 	}
 	public void setCase(Case contractingCase){
-		
+		this.contractingCase=contractingCase;
 	}
 	public void complete(){
-		
+		contract= new Contract();
+		contract.setCustomer(contractingCase.getCustomer());
+		contract.setCars(customerRequirements.getCars());
 	}
 	public Contract getContract(){
 		return null;
