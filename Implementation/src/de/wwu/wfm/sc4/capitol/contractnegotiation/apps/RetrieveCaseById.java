3bcd4d9c934a1e1580c421d354150d51b8143591
marvinfranke <marvin.franke@uni-muenster.de>
2014-01-10 20:17:21 +0100

    RetrieveCaseByID and Extract CustomerReqirements updated
    RetrieveCaseByID and Extract CustomerReqirements coded with a frew open
    questions "unsolved" because of differences at datatypes between dto and
    our data structure

--- Implementation/src/de/wwu/wfm/sc4/capitol/contractnegotiation/apps/RetrieveCaseById.java
+++ Implementation/src/de/wwu/wfm/sc4/capitol/contractnegotiation/apps/RetrieveCaseById.java
@@ -1,23 +1,17 @@
 package de.wwu.wfm.sc4.capitol.contractnegotiation.apps;
 
 import de.wwu.wfm.sc4.capitol.data.Case;
+import de.wwu.wfm.sc4.capitol.service.ServiceInitializer;
 
 public class RetrieveCaseById {
-	
-	public RetrieveCaseById(){
-		
-	}
+	private int caseID;
 	
 	public void setID(Integer id){
-		
+		caseID = id;
 	}
 	
-	public void complete(){
-		
-	}
-	
-	public Case getCase(){
-		return null;
+	public Case complete(){
+		return ServiceInitializer.getProvider().getCaseService().findById(caseID);
 	}
 
 }
