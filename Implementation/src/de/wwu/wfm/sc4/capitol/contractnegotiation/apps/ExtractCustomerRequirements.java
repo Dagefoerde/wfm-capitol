37394ef22765c55a904136bd19d3b4219d6e5fb0
marvinfranke <marvin.franke@uni-muenster.de>
2014-01-10 20:40:01 +0100

    Revert "RetrieveCaseByID and Extract CustomerReqirements updated"
    This reverts commit 3bcd4d9c934a1e1580c421d354150d51b8143591.

--- Implementation/src/de/wwu/wfm/sc4/capitol/contractnegotiation/apps/ExtractCustomerRequirements.java
+++ Implementation/src/de/wwu/wfm/sc4/capitol/contractnegotiation/apps/ExtractCustomerRequirements.java
@@ -1,35 +1,23 @@
 package de.wwu.wfm.sc4.capitol.contractnegotiation.apps;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
-//import ContractData.Car;
 import DTO.DataTransferObject;
-import de.wwu.wfm.sc4.capitol.data.Car;
 import de.wwu.wfm.sc4.capitol.data.Requirements;
 
 public class ExtractCustomerRequirements {
-	private DataTransferObject dto;
-	private Requirements req;
+	
+	public ExtractCustomerRequirements(){
+		
+	}
 	
 	public void setDataTransferObject(DataTransferObject dto){
-		this.dto = dto;
+		
 	}
 	
 	public void complete(){
-		req = new Requirements();
-		Collection <Car> cars = new ArrayList<Car>();
-		for(int i = 0; i<dto.getContractData().getRequirements().getCars().size(); i++)
-			cars.add(new Car(dto.getContractData().getRequirements().getCars().get(i).getLicensePlate(),
-					dto.getContractData().getRequirements().getCars().get(i).getType(), 
-					dto.getContractData().getRequirements().getCars().get(i).getColor(), 
-					dto.getContractData().getRequirements().getCars().get(i).getValue()));
-		req.setCars(cars);
-		req.setHumanInsured(dto.getContractData().getRequirements().getHumanInsured());
-		//req.setNaturalInsured(dto.getContractData().getRequirements().getNaturalInsured());
-		req.setPickupService(dto.getContractData().getRequirements().isPickUpService());
+		
 	}
 	public Requirements getCustomerRequirements(){
-		return req;
+		return null;
 	}
+
 }
--- Implementation/src/de/wwu/wfm/sc4/capitol/contractnegotiation/apps/RetrieveCaseById.java
+++ Implementation/src/de/wwu/wfm/sc4/capitol/contractnegotiation/apps/RetrieveCaseById.java
@@ -1,17 +1,23 @@
 package de.wwu.wfm.sc4.capitol.contractnegotiation.apps;
 
 import de.wwu.wfm.sc4.capitol.data.Case;
-import de.wwu.wfm.sc4.capitol.service.ServiceInitializer;
 
 public class RetrieveCaseById {
-	private int caseID;
 	
-	public void setID(Integer id){
-		caseID = id;
+	public RetrieveCaseById(){
+		
 	}
 	
-	public Case complete(){
-		return ServiceInitializer.getProvider().getCaseService().findById(caseID);
+	public void setID(Integer id){
+		
+	}
+	
+	public void complete(){
+		
+	}
+	
+	public Case getCase(){
+		return null;
 	}
 
 }
