7b09953f6fe1cd3328340a093962a509d14478e8
Dexter-03 <tobias.reischmann@googlemail.com>
2014-01-16 17:33:17 +0100

    Cleaned up unused rows

--- Implementation/src/de/wwu/wfm/sc4/capitol/contractnegotiation/apps/SaveWorkflowDataForCR.java
+++ Implementation/src/de/wwu/wfm/sc4/capitol/contractnegotiation/apps/SaveWorkflowDataForCR.java
@@ -2,11 +2,6 @@ package de.wwu.wfm.sc4.capitol.contractnegotiation.apps;
 
 import de.wwu.wfm.sc4.capitol.data.Case;
 import de.wwu.wfm.sc4.capitol.service.ServiceInitializer;
-import de.wwu.wfm.sc4.capitol.data.Requirements;
-import de.wwu.wfm.sc4.capitol.data.Car;
-import de.wwu.wfm.sc4.capitol.data.Contract;
-
-import java.util.*;
 
 public class SaveWorkflowDataForCR {
 	private Case case0;
@@ -16,31 +11,6 @@ public class SaveWorkflowDataForCR {
 	}
 	public void complete(){
 		ServiceInitializer.getProvider().getCaseService().persist(case0);
-		
-		/*ServiceInitializer.getProvider().getCustomerService().persist(case0.getCustomer());
-		
-		Collection<Requirements> reqs = case0.getRequirements();
-		Requirements currentReq = null;
-		Iterator<Requirements> iteratorReq =reqs.iterator();
-		while (iteratorReq.hasNext())
-		currentReq=iteratorReq.next();
-		ServiceInitializer.getProvider().getRequirementsService().persist(currentReq);
-		
-		
-		Collection<Car> cars = currentReq.getCars();
-		Car currentCar = null;
-		Iterator<Car> iteratorCar =cars.iterator();
-		while (iteratorCar.hasNext())
-		currentCar = iteratorCar.next();
-		ServiceInitializer.getProvider().getCarService().persist(currentCar);
-		
-		
-		Collection<Contract> cons = case0.getContract();
-		Contract currentCon = null;
-		Iterator<Contract> iteratorCon = cons.iterator();
-		while (iteratorCon.hasNext())
-		currentCon=iteratorCon.next();
-		ServiceInitializer.getProvider().getContractService().persist(currentCon);*/
 	}
 
 }
