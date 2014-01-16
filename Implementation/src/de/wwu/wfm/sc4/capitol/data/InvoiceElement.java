06a24e46df3dff20127fdc2f41ff961428aa692e
the-Other-One <jan.dagefoerde@googlemail.com>
2014-01-16 11:33:59 +0100

    Revert "invoiceelement hibernate conf"
    This reverts commit af4853631ccc621af476bde2885a22f59f7ceae1.

--- Implementation/src/de/wwu/wfm/sc4/capitol/data/InvoiceElement.java
+++ Implementation/src/de/wwu/wfm/sc4/capitol/data/InvoiceElement.java
@@ -15,7 +15,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "invoice_element")
-public class InvoiceElement extends AbstractDataClass {
+public class InvoiceElement {
 
 	private static final long serialVersionUID = 12L;
 
