53abb48861bb2281791a7d9e600cd5ade3e161cc
Dexter-03 <tobias.reischmann@googlemail.com>
2014-01-16 17:32:58 +0100

    The customer is now loaded from the database if a second case is opened for him

--- Implementation/src/de/wwu/wfm/sc4/capitol/contractnegotiation/apps/TransferDataFromCustomerRequirementsToCase.java
+++ Implementation/src/de/wwu/wfm/sc4/capitol/contractnegotiation/apps/TransferDataFromCustomerRequirementsToCase.java
@@ -1,12 +1,9 @@
 package de.wwu.wfm.sc4.capitol.contractnegotiation.apps;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 import DTO.DataTransferObject;
-import de.wwu.wfm.sc4.capitol.data.Car;
 import de.wwu.wfm.sc4.capitol.data.Case;
 import de.wwu.wfm.sc4.capitol.data.Customer;
+import de.wwu.wfm.sc4.capitol.service.ServiceInitializer;
 
 public class TransferDataFromCustomerRequirementsToCase {
 
@@ -29,26 +26,26 @@ public class TransferDataFromCustomerRequirementsToCase {
 
 	public void complete() {
 		case0=new Case();
-		Customer cust = new Customer(dto.getCustomer().getFirstName(), dto
+		Customer customer;
+		if (dto.getCustomer()!=null){
+		customer = ServiceInitializer.getProvider().getCustomerService().findByUsername(dto.getCustomer().getUsername());
+		//Update Data
+		customer.setFirstname(dto.getCustomer().getFirstName());
+		customer.setLastname(dto.getCustomer().getLastName());
+		customer.setUsername(dto.getCustomer().getUsername());
+		customer.setPhone(dto.getCustomer().getPhone());		
+		customer.setStreet(dto.getCustomer().getAddress().getStreet());
+		customer.setStreetNumber(dto.getCustomer().getAddress().getStreetNumber());
+		customer.setPostalCode(dto.getCustomer().getAddress().getPostalCode());		
+		}
+		else {
+		customer = new Customer(dto.getCustomer().getFirstName(), dto
 				.getCustomer().getLastName(), dto.getCustomer().getUsername(),
 				dto.getCustomer().getPhone());
-		cust.setStreet(dto.getCustomer().getAddress().getStreet());
-		cust.setStreetNumber(dto.getCustomer().getAddress().getStreetNumber());
-		cust.setPostalCode(dto.getCustomer().getAddress().getPostalCode());
-		case0.setCustomer(cust);
-
-		Collection<Car> cars = new ArrayList<Car>();
-		for (int i = 0; i < dto.getContractData().getRequirements().getCars()
-				.size(); i++)
-			cars.add(new Car(dto.getContractData().getRequirements().getCars()
-					.get(i).getLicensePlate(), dto.getContractData()
-					.getRequirements().getCars().get(i).getType(), dto
-					.getContractData().getRequirements().getCars().get(i)
-					.getColor(), dto.getContractData().getRequirements()
-					.getCars().get(i).getValue()));
-		// TODO The collection "cars" is not assigned anywhere! What is it good for? 
-		// Aren't cars part of the Requirements (and therefore not necessary/sensible here)?
-		// Also, they will be part of the (final) contract, but not of the customer directly...
+		customer.setStreet(dto.getCustomer().getAddress().getStreet());
+		customer.setStreetNumber(dto.getCustomer().getAddress().getStreetNumber());
+		customer.setPostalCode(dto.getCustomer().getAddress().getPostalCode());}
+		case0.setCustomer(customer);
 	}
 
 }
