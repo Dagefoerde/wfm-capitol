4852a565c89c82113b8a08fa737cbf1700ba3118
the-Other-One <jan.dagefoerde@googlemail.com>
2014-01-16 12:31:31 +0100

    coverage decision jsf

--- Implementation/src/de/wwu/wfm/sc4/capitol/insuranceclaim/apps/CreateCoverageDecisionBean.java
+++ Implementation/src/de/wwu/wfm/sc4/capitol/insuranceclaim/apps/CreateCoverageDecisionBean.java
@@ -1,5 +1,23 @@
 package de.wwu.wfm.sc4.capitol.insuranceclaim.apps;
+
+
+import de.wwu.wfm.sc4.capitol.data.Incident;
+
 public class CreateCoverageDecisionBean {
-  public void complete(){
-  }
+	private Incident incident;
+
+	public CreateCoverageDecisionBean() {
+	}
+
+	public void complete() {
+	}
+
+	public void setIncident(Incident incident) {
+		this.incident = incident;
+	}
+
+	public Incident getIncident() {
+		return incident;
+	}
+
 }
