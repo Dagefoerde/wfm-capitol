e9ca69e6f03f0a6732a7290b4b2959a06877a327
Dexter-03 <tobias.reischmann@googlemail.com>
2014-01-23 11:51:50 +0100

    Introduced CapitolConstants which include the paths in the workingDirectory where the contracts are safed.

---
+++ Implementation/src/de/wwu/wfm/sc4/capitol/constants/CapitolConstants.java
@@ -0,0 +1,11 @@
+package de.wwu.wfm.sc4.capitol.constants;
+
+public class CapitolConstants {
+
+	public static final String SETTINGS_PATH="Settings";
+	
+	public static final String CONTRACTS_PATH="Contracts";
+	
+	public static final String PRELIMINARY_CONTRACTS_PATH="PreliminaryContracts";
+	
+}
