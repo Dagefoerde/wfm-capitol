9e406a3706ce7c7af4cde57eaf8fb748f2626e3b
Dexter-03 <tobias.reischmann@googlemail.com>
2014-01-24 10:47:02 +0100

    CAR SERVICE CHECKS FOR LICENSE PLATE SRY FOR CAPS

--- Implementation/src/de/wwu/wfm/sc4/capitol/service/CarService.java
+++ Implementation/src/de/wwu/wfm/sc4/capitol/service/CarService.java
@@ -1,9 +1,14 @@
 package de.wwu.wfm.sc4.capitol.service;
 
+import java.util.Calendar;
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 
 import de.wwu.wfm.sc4.capitol.data.Car;
+import de.wwu.wfm.sc4.capitol.data.Incident;
 
 public class CarService extends AbstractServiceClass<Car> {
 	protected CarService() {
@@ -16,7 +21,15 @@ public class CarService extends AbstractServiceClass<Car> {
 
 	public Car findByLicencePlate(String licencePlate) {
 		Session s = ServiceInitializer.getProvider().getSession();
-		return (Car) s.createCriteria(Car.class).add(
-				Restrictions.eq("licencePlate", licencePlate)).uniqueResult();
+		Query q = s.createQuery("from Car c" +
+				" where c.licencePlate = :licencePlate" +
+				" and c.contract.startDate < :today and c.contract.endDate > :today ");
+		q.setString("licencePlate", licencePlate);
+		q.setDate("today", Calendar.getInstance().getTime()); // TODO Date of accident, not today!! --> parameter; maybe second method.
+		
+		List<?> results = q.list();
+		
+		if (results.size() == 0) return null;
+		return (Car)results.get(0);	
 	}
 }
